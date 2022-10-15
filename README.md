0c50b03b10535075dd323bcd0bf489bbbb792bba
Sujay1844 <sujay1844@protonmail.com>
2022-10-15 19:51:32 +0530

    Updated documentation

---
+++ README.md
@@ -0,0 +1,15 @@
+This is a script to automate Whatsapp messaging
+For now, it supports only Firefox
+
+To run it,
+- Install `python`
+- Install dependencies,
+```
+pip install -r requirements.txt
+```
+- Change the contact, message and firefox profile path in the beginning of the script.
+Navigate to `about:profiles` in Firefox to find your profile path
+- Now run the script,
+```
+python whatsapp-bot.py
+```
--- whatsapp-bot.py
+++ whatsapp-bot.py
@@ -1,42 +1,58 @@
 #!/usr/bin/env python
 
+# This is a script to automate Whatsapp messaging
+# For now, it supports only Firefox
+
+contact = ""
+msg = ""
+profile_path = ""
+
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
-# from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-contact = "Skandan Schl"
-msg = "This message was sent by a bot"
+try:
+    # Importing variable from .env
+    from dotenv import load_dotenv
+    import os
+    load_dotenv()
+    contact = os.getenv("CONTACT")
+    msg = os.getenv("MESSAGE")
+    profile_path = os.getenv("PROFILE_PATH")
+except Exception as err:
+    if profile_path == "" or contact == "" or msg == "":
+        print("Change contact, msg and profile_path variables at the beginning of this script")
+        exit()
 
-profile_path = "/home/sujay1844/.mozilla/firefox/y4gt43vs.default-release"
+# If you don't want to use your default profile, you need to scan QR code every time
 opts = webdriver.FirefoxOptions()
-opts.add_argument("--profile "+ profile_path)
+opts.add_argument("--profile " + profile_path)
 browser = webdriver.Firefox(options=opts)
 
+# Navigate to Whatsapp Web
 browser.get("https://web.whatsapp.com")
 
-# input("Scan QR Code and press enter...")
-# print("Logged in (hopefully)...")
-
-search_box_xpath = "/html/body/div[1]/div/div/div[3]/div/div[1]/div/div/div[2]/div/div[2]"
+# Search for the contact
+search_xpath = "/html/body/div[1]/div/div/div[3]/div/div[1]/div/div/div[2]/div/div[2]"
+# The script would complain that element not found
+# It won't be found because it is not loaded yet
+# The following line waits for the page to load
 search_box = WebDriverWait(browser, 50).until(lambda browser:
-                                browser.find_element(By.XPATH, search_box_xpath))
-
+                                                  browser.find_element(By.XPATH, search_xpath))
 search_box.click()
 search_box.send_keys(contact)
 search_box.send_keys(Keys.ENTER)
 
 # Type in the message and press enter
-message_box_xpath = "/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]"
-message_box = WebDriverWait(browser, 50).until(lambda browser:
-                                browser.find_element(By.XPATH, message_box_xpath))
-message_box.click()
+msg_box_xpath = "/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]"
+msg_box = WebDriverWait(browser, 50).until(lambda browser:
+                                                   browser.find_element(By.XPATH, msg_box_xpath))
+msg_box.click()
 for x in msg:
-    message_box.send_keys(x)
+    msg_box.send_keys(x)
 input("Press enter to send message...")
-message_box.send_keys(Keys.ENTER)
+msg_box.send_keys(Keys.ENTER)
 
-
-input("Press enter to quit")
+input("Press enter to quit...")
 browser.close()
